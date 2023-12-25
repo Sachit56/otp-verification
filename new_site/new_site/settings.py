3fbacca5147e0ae330d416bb00199c16985b49e7
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-25 17:16:12 +0545

    auth

--- new_site/new_site/settings.py
+++ new_site/new_site/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'new_api'
+    'new_api',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
--- new_site/new_site/urls.py
+++ new_site/new_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('new_api.urls'))
+    path('',include('new_api.urls')),
+    path('api-authtoken/',views.obtain_auth_token)
 ]
