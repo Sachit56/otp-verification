4c30cca4a9728b5556d87a43415bc77270c5c0c0
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-25 16:27:27 +0545

    tokenizations

--- new_site/new_api/urls.py
+++ new_site/new_api/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name='new_api'
 
 urlpatterns = [
-    path('',views.apiView,name='api'),
-    path('post/',views.Post_View,name='post'),
-    path('update/<int:id>',views.Update_View,name='patch'),
-    path('delete/<int:id>',views.Delete_View,name='delete'),
-    path('book/',views.BookView,name='book')
+    path('<int:id>/',views.StudentView.as_view(),name='api-id'),
+    path('book/',views.BookView,name='book'),
+    path('',views.StudentView.as_view(),name='api')
 ]
--- new_site/new_api/views.py
+++ new_site/new_api/views.py
@@ -1,54 +1,57 @@
 from django.shortcuts import render,get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,APIView
 from .models import *
 from .serializers import *
+
 # Create your views here.
+class StudentView(APIView):
+    def get(self,request):
+        students=Student.objects.all()
+        serializer=StudentSerializer(students,many=True)
 
-@api_view(['GET'])
-def apiView(request):
-    students=Student.objects.all()
-    serializer=StudentSerializer(students,many=True)
+        return Response({'status':200,'students':serializer.data})
+    def post(self,request):
+        data=request.data
+        serializer=StudentSerializer(data=data)
 
-    return Response({'status':200,'students':serializer.data})
+        if not serializer.is_valid():
+            print(serializer.errors)
+            return Response({'status':404,'message':'Not found'})
 
-@api_view(['POST'])
-def Post_View(request):
-    data=request.data
-    serializer=StudentSerializer(data=data)
-
-    if not serializer.is_valid():
-        print(serializer.errors)
-        return Response({'status':404,'message':'Not found'})
-
-    serializer.save()
-    return Response({'status':200,'message':serializer.data})
-
-@api_view(['PATCH'])
-def Update_View(request,id):
-    data=request.data
-    student_obj = get_object_or_404(Student, id=id)
-    serializer=StudentSerializer(student_obj,data=data,partial=True)
-
-    if not serializer.is_valid():
-        print(serializer.errors)
-        return Response({'status':400,'msg':'no such datas'})
+        serializer.save()
+        return Response({'status':200,'message':serializer.data})
     
-    serializer.save()
-    return Response({'status':200,'message':serializer.data})
+    def patch(self,request,*args, **kwargs):
+        id=kwargs.get("id")
+        data=request.data
+        student_obj = get_object_or_404(Student, id=id)
+        serializer=StudentSerializer(student_obj,data=data,partial=True)
 
-@api_view(['DELETE'])
-def Delete_View(request,id):
-    try:
-        student_obj=get_object_or_404(Student,id=id)
-        student_obj.delete()
-        return Response({'status':400,'message':'Deleted'})
+        if not serializer.is_valid():
+            print(serializer.errors)
+            return Response({'status':400,'msg':'no such datas'})
+        
+        serializer.save()
+        return Response({'status':200,'message':serializer.data})
+
+    def delete(self,request,id):
+        try:
+            student_obj=get_object_or_404(Student,id=id)
+            student_obj.delete()
+            return Response({'status':400,'message':'Deleted'})
     
 
-    except:
+        except:
         
   
-        return Response({'status':200,'message':'Cannot delete'})
+            return Response({'status':200,'message':'Cannot delete'})
+
+
+
+
+
+
 
 @api_view(['GET'])
 def BookView(request):
